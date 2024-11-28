0fee90aae9accb49c5c7df93934762013136d6e3
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-11-28 09:49:47 +0530

    added routes and rate-limiting for no abuse of the api

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,22 @@ import express from 'express';
 import connectDB from './config/db';
 import dotenv from 'dotenv'
 import urlRoutes from './routes/urlRoutes';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 
 dotenv.config();
 
 connectDB();
+
+app.use(express.json());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 50 // limit each IP to 50 requests per windowMs
+});
+
+app.use(limiter);
 
 app.get('/', (req, res) => {
   res.send('URL Shortening service is running!');
--- src/routes/urlRoutes.ts
+++ src/routes/urlRoutes.ts
@@ -1,7 +1,24 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { redirectURL, shortenURL } from '../controllers/urlController';
 
 const router = express.Router();
 
+router.post('/shorten', async (req: Request, res: Response) => {
+    try {
+        const response = await shortenURL(req, res);
+        return response;
+    } catch (error) {
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
+router.get('/:shortUrl', async (req: Request, res: Response) => {
+    try {
+        const response = await redirectURL(req, res);
+        return response;
+    } catch (error) {
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 export default router;
